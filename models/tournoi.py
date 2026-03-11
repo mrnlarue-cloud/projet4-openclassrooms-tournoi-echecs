47a0488cca820f79457287164eb6d6e09623c52a
Marion.L <mrn.larue@gmail.com>
2026-03-11 08:57:28 +0100

    Ajout de la sérialisation de la classe Tournoi

--- models/tournoi.py
+++ models/tournoi.py
@@ -1,3 +1,7 @@
+from .joueur import Joueur
+from .tour import Tour
+
+
 # Définition de la classe Tournoi.
 # Cette classe représente un tournoi d'échecs.
 class Tournoi:
@@ -29,6 +33,58 @@ class Tournoi:
     def ajouter_tour(self, tour):
         # Ajoute un objet Tour dans la liste des tours du tournoi.
         self.tours.append(tour)
+
+    def to_dict(self):
+        # Cette méthode transforme l'objet Tournoi en dictionnaire.
+        # Le but est d'obtenir une structure simple compatible avec JSON.
+
+        return {
+            # On enregistre les informations simples du tournoi.
+            "nom": self.nom,
+            "lieu": self.lieu,
+            "date_debut": self.date_debut,
+            "date_fin": self.date_fin,
+            "description": self.description,
+            "nombre_tours": self.nombre_tours,
+            # self.joueurs contient des objets Joueur.
+            # On convertit donc chaque joueur en dictionnaire.
+            "joueurs": [joueur.to_dict() for joueur in self.joueurs],
+            # self.tours contient des objets Tour.
+            # On convertit donc chaque tour en dictionnaire.
+            "tours": [tour.to_dict() for tour in self.tours],
+        }
+
+    @classmethod
+    def from_dict(cls, data):
+        # Cette méthode recrée un objet Tournoi à partir d'un dictionnaire.
+        # "cls" représente ici la classe Tournoi.
+
+        # On recrée d'abord l'objet principal avec les données nécessaires
+        # au constructeur __init__.
+        tournoi = cls(
+            data["nom"],
+            data["lieu"],
+            data["date_debut"],
+            data["description"],
+            data["nombre_tours"],
+        )
+
+        # La date de fin n'est pas dans le constructeur,
+        # donc on la remet à part après la création de l'objet.
+        tournoi.date_fin = data["date_fin"]
+
+        # data["joueurs"] contient une liste de dictionnaires.
+        # Chaque dictionnaire représente un joueur.
+        # On reconstruit donc chaque objet Joueur.
+        tournoi.joueurs = [Joueur.from_dict(joueur) for joueur in data["joueurs"]]
+
+        # data["tours"] contient une liste de dictionnaires.
+        # Chaque dictionnaire représente un tour.
+        # On reconstruit donc chaque objet Tour.
+        tournoi.tours = [Tour.from_dict(tour) for tour in data["tours"]]
+
+        # On renvoie l'objet Tournoi complètement reconstruit.
+        return tournoi
 
     def __str__(self):
         # Méthode spéciale appelée lorsque l'on fait print(tournoi).
