6c72770512c2b05162d5b76b8baf9c0193dfd51a
Marion.L <mrn.larue@gmail.com>
2026-03-23 10:51:35 +0100

    Modification des commentaires par des docstrings.

--- main_projet_4_oc.py
+++ main_projet_4_oc.py
@@ -1,7 +1,6 @@
-# Import du contrôleur du menu principal.
+"""Import du contrôleur du menu principal."""
 from controllers.menu_controleur import lancer_menu_principal
 
-# Point d'entrée du programme.
-# Le main lance simplement le contrôleur principal.
+""" Point d'entrée du programme."""
 if __name__ == "__main__":
     lancer_menu_principal()
